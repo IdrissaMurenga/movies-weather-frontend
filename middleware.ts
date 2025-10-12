2b49baa4c68f162e23ebce03a61d48036fc4ba30
idrissa murenga <1mulengaidriss@gmail.com>
2025-10-12 02:47:45 +0200

    fixed app router

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,5 @@ export { auth as middleware } from "./libs/auth";
 
 // Run middleware on all routes except static/assets
 export const config = {
-    matcher: [
-        "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-    ],
+    matcher: ["/dashboard/:path*",],
 };
